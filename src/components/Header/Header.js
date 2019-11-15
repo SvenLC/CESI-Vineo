0ceb81575a7baabe49a9ca9f530f9ea8e01cc957
SvenLC <sven.lecann@live.fr>
2019-11-15 10:37:14 +0100

    header + navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './Header.module.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import logoVineo from '../../assets/images/icons/logoVineo.png'
 
-const navigationItems = (props) => (
-    <header className={styles.header}>
-        <h1>Header</h1>
-        <Link to='/' className={styles.link}>Accueil</Link>
-    </header>
-    
-);
-export default navigationItems;
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '', toto: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+        console.log(event.target.value)
+        // console.log(this.state.value);
+    }
+
+
+    render() {
+        return (
+            <header className={styles.entete}>
+                <img src={logoVineo} />
+                <input className={styles.inputText} type="text" size={"35"} placeholder={"Recherche"} onChange={this.handleChange}></input>
+                <div className={styles.topRight}>
+                    <Link className={`${styles.bouton} ${styles.fav}`} to='/'>
+                        Favoris
+            </Link>
+
+                    <div className={`${styles.bouton} ${styles.cart}`}>Panier</div> 
+                    <div className={`${styles.bouton} ${styles.cont}`}>Contact</div>
+                    <div className={styles.compte}>
+                        <div className={`${styles.bouton} ${styles.log}`}>Connexion</div>
+                        <div className={`${styles.bouton} ${styles.log}`}>Inscription</div>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
+export default Header;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import styles from './Navbar.module.css';
 
 
 const navbar = (props) => (
-    <header className={styles.Navbar}>
-        <h1>Navbar</h1>
-    </header>
-    
+    <nav className={styles.menu}>
+        <ul>
+            <li><Link className={styles.child} to='/menu'>Accueil</Link></li>
+            <li><Link className={styles.child} to='/catalogue'>Catalogue</Link></li>
+            <li><Link className={styles.child} to='/rouge'>Rouge</Link></li>
+            <li><Link className={styles.child} to='/blanc'>Blanc</Link></li>
+            <li><Link className={styles.child} to='/rose'>Rosé</Link></li>
+            <li><Link className={styles.child} to='/champagne'>Champagne</Link></li>
+        </ul>
+    </nav>
+
 );
+
 export default navbar;
